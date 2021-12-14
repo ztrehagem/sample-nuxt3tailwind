ec9761d0a6ce79de0d74dba202315565476d276c
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2021-12-14 21:31:10 +0900

    commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p class="text-red-500">Hello</p>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './*.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
